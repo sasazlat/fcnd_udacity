0725591dd239e94b3a1bbc55797669a52621f603
sasazlat <sashsoft@gmail.com>
2018-03-07 09:03:27 +0100

    backyard_flyer started

--- preview_drone/ProjectBackyardFlyer/backyard_flyer.py
+++ preview_drone/ProjectBackyardFlyer/backyard_flyer.py
@@ -36,38 +36,51 @@ class BackyardFlyer(Drone):
         self.register_callback(MsgID.STATE, self.state_callback)
 
     def local_position_callback(self):
+        # TODO: Implement this method
         """
-        TODO: Implement this method
-
         This triggers when `MsgID.LOCAL_POSITION` is received and self.local_position contains new data
         """
+        if self.flight_state == States.TAKEOFF:
+            altitude = -1 * self.local_position[2]
+            if altitude > 0.95 * self.target_position[2]:
+                self.calculate_box()
+                self.waypoint_transition()
+
+
         pass
 
     def velocity_callback(self):
+        #TODO: Implement this method
         """
-        TODO: Implement this method
 
         This triggers when `MsgID.LOCAL_VELOCITY` is received and self.local_velocity contains new data
         """
         pass
 
     def state_callback(self):
+        #TODO: Implement this method
         """
-        TODO: Implement this method
 
         This triggers when `MsgID.STATE` is received and self.armed and self.guided contain new data
         """
         pass
 
     def calculate_box(self):
-        """TODO: Fill out this method
+        #TODO: Fill out this method
+        """
         
         1. Return waypoints to fly a box
         """
-        pass
+        box = [(10, 0, 5, 0),
+                (10, 10, 5, 0),
+                (0, 10, 5, 0),
+                (0, 0, 5, 0)]
+        self.all_waypoints.append(box)
+        
 
     def arming_transition(self):
-        """TODO: Fill out this method
+        #TODO: Fill out this method
+        """
         
         1. Take control of the drone
         2. Pass an arming command
@@ -75,39 +88,62 @@ class BackyardFlyer(Drone):
         4. Transition to the ARMING state
         """
         print("arming transition")
+        drone.take_control()
+        dron.arm()
+
+        drone.set_home_position(self.global_position[0],
+                                self.global_position[1],
+                                self.global_position[2])
+
+        self.flight_state = States.ARMING
 
     def takeoff_transition(self):
-        """TODO: Fill out this method
+        #TODO: Fill out this method
+        """
         
         1. Set target_position altitude to 3.0m
         2. Command a takeoff to 3.0m
         3. Transition to the TAKEOFF state
         """
         print("takeoff transition")
+        target_altitude = 5.0
+        self.target_position[2] = target_altitude
+        self.takeoff(target_altitude)
+        self.flight_state = States.TAKEOFF
 
     def waypoint_transition(self):
-        """TODO: Fill out this method
+        #TODO: Fill out this method
+        """
     
         1. Command the next waypoint position
         2. Transition to WAYPOINT state
         """
         print("waypoint transition")
+        
+        self.flight_state = States.WAYPOINT
 
     def landing_transition(self):
-        """TODO: Fill out this method
+        #TODO: Fill out this method
+        """
         
         1. Command the drone to land
         2. Transition to the LANDING state
         """
         print("landing transition")
+        self.land()
+        self.flight_state = States.LANDING
 
     def disarming_transition(self):
-        """TODO: Fill out this method
+        #TODO: Fill out this method
+        """
         
         1. Command the drone to disarm
         2. Transition to the DISARMING state
         """
         print("disarm transition")
+        self.disarm()
+        self.release_control()
+        self.flight_state = States.MANUAL
 
     def manual_transition(self):
         """This method is provided
